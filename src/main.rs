b8b5b345eb65ef53ef05908bca16d65722b70c23
owen <stensonowen@gmail.com>
2016-07-22 05:08:23 -0400

    src/ast.rs

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn main() {
     assert!(croncfg::parse_Line("* * * * * http://www.google.com").is_ok());
     assert!(croncfg::parse_Line("1 2 3 4 5 https://google.com").is_ok());
     println!("{:?}", croncfg::parse_Line("1 2 3 4 5 https://google.com").unwrap());
-    //println!("{:?}", croncfg::parse_Line("1 * L 4-9,11-12,13,* */4 https://ddg.co").unwrap());
     println!("{:?}", croncfg::parse_Line("1*2\t4*/4https://bing.com").unwrap());
     println!("{:?}", croncfg::parse_Line("*****https://reddit.com").unwrap());
+    println!("{:?}", croncfg::parse_Line("@yearly https://ddg.co").unwrap());
 }
