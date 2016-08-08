c99d2e918ce4711d37ddf2a5267fcb21485ca9ff
owen <stensonowen@gmail.com>
2016-08-08 03:25:41 -0400

    parse() should mostly work. need to test though. how variables end up being handled is dependent on https://github.com/nikomatsakis/lalrpop/issues/127

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -21,21 +21,73 @@
 //Parse all of the data from the input file more comprehensively than
 // ast.rs can on its own
 
-//mod event;
-//mod croncfg;
-//use croncfg;
-//use self::croncfg;
-//pub mod ast;
-//use ast::*;
-
+#[allow(dead_code)]
 pub mod croncfg;
+#[allow(dead_code)]
 pub mod ast;
 
-//use event::pushjet::{load_config, contact};
-//use action::pushjet::{load_config, contact};
-//use action::scrape::*;
+use std::collections::HashMap;
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::error::Error;
 
-pub fn parse() {
-    let test = croncfg::parse_Line("*****http://gnu.org");
+
+
+//pub fn parse(input: String) {
+pub fn parse(input: &Path) -> Result<(Vec<ast::Command>,HashMap<&str,String>),Vec<String>> {
+    //all goes well, return a tuple of all commands and all variables
+    //if there are parsing errors, return a vector of of them
+    let file = match File::open(input) {
+        Ok(f)  => f,
+        Err(e) => return Err(vec![format!("Failed to open config file for parsing: {}",
+                                          e.description()).to_string()]),
+    };
+    //keep track of lines:
+    let mut variables = HashMap::<&str,String>::new();
+    let mut commands  = Vec::<ast::Command>::new();
+    let mut errors    = Vec::<String>::new();
+    //iterate through config by newline
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        //unwrap line
+        let buffer = match line {
+            Ok(l)  => l,
+            Err(e) => {
+                let err = format!("Failed to read line from config: {}", e.description());
+                errors.push(err);
+                continue
+            }
+        };
+        //parse line
+        let parsed = match croncfg::parse_Line(&buffer) {
+            Ok(l)  => l,
+            Err(e) => {
+                let err = format!("Failed to parse line `{}` from config: {:?}", buffer, e);
+                errors.push(err);
+                continue
+            }
+        };
+        //organize result
+        match parsed {
+            ast::Line::Cmd(cmd) => commands.push(cmd),
+            ast::Line::VarSet(v)=> {
+                let pair = match v {
+                    ast::Var::Email(u)    => ("EMAIL",  u),
+                    ast::Var::PjSecret(u) => ("SECRET", u),
+                    ast::Var::PjUrl(u)    => ("URL",    u),
+                    ast::Var::DataDir(u)  => ("DIR",    u),
+                };
+                variables.insert(pair.0, pair.1);
+            },
+            ast::Line::Comment  => (),
+        };
+    }
+    //return errors or content
+    if errors.is_empty() {
+        Ok((commands, variables))
+    } else {
+        Err(errors)
+    }
 }
 
