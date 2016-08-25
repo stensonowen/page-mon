96696e9d412a34325925b0621504f3b531e0b312
Owen Stenson <owen@stenso-deb-pc.qjkxkcd.xyz>
2016-08-24 21:41:15 -0400

    Added job::fire. This project is turning into mostly abstractions at this point

--- src/action/scrape.rs
+++ src/action/scrape.rs
@@ -26,6 +26,7 @@ extern crate select;
 use std::fs::File;
 use std::error::Error;
 use std::io::{Read, Write};
+use std::path::PathBuf;
 
 use self::hyper::header::*;
 use self::select::predicate::Name;
@@ -85,11 +86,14 @@ fn decompose_and_diff(old: &str, new: &str) -> String {
 }
 
 
+/*
 pub fn compare(url: hyper::Url, path: &str) -> Result<String,String> {
     //`old` has been converted to bytes, is that a problem?
     //TODO: verify old cache file exists before this
     //let filename = url_to_str(&url);
     //create buffers for the `old` and `new` htmls, and attempt to load them
+    //BUG: we need the full difference also
+    //DEPRECATED
     let mut old_txt = String::new();
     if let Err(e) = get_cache(path, &mut old_txt) {
         return Err(format!("Failed to open cache: {}", e))
@@ -106,7 +110,19 @@ pub fn compare(url: hyper::Url, path: &str) -> Result<String,String> {
         false => rhs_of_diff,
     };
     Ok(diff_fn(&old_txt, &new_txt))
+}*/
+
+pub fn diff(old: &str, new: &str) -> String {
+    //master differ
+    //decide which diff fn to use and use it
+    //TODO: better way to decide which way to diff
+    let diff_fn = match new.len() > DIFF_THRESHOLD {
+        true  => decompose_and_diff,
+        false => rhs_of_diff,
+    };
+    diff_fn(&old, &new)
 }
+
 
 /*
 pub fn set_cache(filename: &str, contents: &str) -> Result<(),String> {
@@ -124,7 +140,14 @@ pub fn set_cache(filename: &str, contents: &str) -> Result<(),String> {
 }
 */
 
-fn get_cache(filename: &str, buffer: &mut String) -> Result<usize,String> {
+/* NOTE:
+ *  Should `get_cache` and `get_url` be public? Or wrapped?
+ *  Job::fire() needs to have possession of both the diff and the `new`,
+ *   so a wrapper would have to return a tuple of results or something
+ */
+
+//pub fn get_cache(filename: &str, buffer: &mut String) -> Result<usize,String> {
+pub fn get_cache(filename: &PathBuf, buffer: &mut String) -> Result<usize,String> {
     //open cached version of a page. Return the html or an error message
     //let filename = url_to_str(url);
     let mut file = match File::open(filename) {
@@ -141,7 +164,7 @@ fn get_cache(filename: &str, buffer: &mut String) -> Result<usize,String> {
     }
 }
 
-fn get_url(url: hyper::Url, buffer: &mut String) -> Result<usize,String> {
+pub fn get_url(url: &hyper::Url, buffer: &mut String) -> Result<usize,String> {
     //perform GET request on url. Return the html or an error message
     let client = hyper::Client::new();
     let mut headers = Headers::new();
@@ -152,7 +175,7 @@ fn get_url(url: hyper::Url, buffer: &mut String) -> Result<usize,String> {
     //Accept-Encoding?
     //Connection keep-alive?
     headers.set(UserAgent(USER_AGENT.to_string()));
-    let res = client.get(url)
+    let res = client.get(url.clone())
                     .headers(headers)
                     .send();
     match res {
